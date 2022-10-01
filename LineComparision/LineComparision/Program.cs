f97235302d0d71401b3d861241c6c75eee3a959e
nrp033 <pawarnitin9433@gmail.com>
2022-10-01 19:16:05 +0530

    Welcome to Line Comparison Computation

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+
+            Console.WriteLine("Enter the value of x1,y1,x2,y2 cordinates");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\nLength of Line : "+Length(x1, y1, x2, y2));
+            Console.ReadLine();
+
+        }
+
+        private static int Length(int x1, int y1, int x2, int y2)
+        {
+            return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+       
+    }
+    
+}
